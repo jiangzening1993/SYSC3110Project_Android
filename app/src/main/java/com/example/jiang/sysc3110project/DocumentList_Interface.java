6acb9463192b322eb3cf1466d7eb586af66a72d8
jiangzening1993 <jiangzening1993@gmail.com>
2015-12-05 21:08:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jiang/sysc3110project/DocumentList_Interface.java
@@ -0,0 +1,7 @@
+package com.example.jiang.sysc3110project;
+
+/**
+ * Created by Jiang on 05/12/2015.
+ */
+public class DocumentList_Interface {
+}
---
+++ app/src/main/java/com/example/jiang/sysc3110project/UserList_Interface.java
@@ -0,0 +1,31 @@
+package com.example.jiang.sysc3110project;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by Jiang on 05/12/2015.
+ */
+public class UserList_Interface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
